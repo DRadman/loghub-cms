e3e31d629951103111545d54e766b9cdccd2215b
Dejan Radmanovic <office@decodex.net>
2024-02-11 13:11:30 +0100

    Vercel Integration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,20 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { APP_INITIALIZER, isDevMode } from "@angular/core";
+import { inject } from "@vercel/analytics";
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideRouter(routes),
+    provideClientHydration(),
+    {
+      provide: APP_INITIALIZER,
+      useFactory: () => {
+        inject({ mode: isDevMode() ? 'development' : 'production' });
+      },
+    },
+  ],
 };
