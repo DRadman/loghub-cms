e08e547026bc5e0cb1f23b52151f17f656ef90fa
Dejan Radmanovic <office@decodex.net>
2024-02-11 13:21:19 +0100

    Vercel Speed Insights

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,6 +2,7 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { APP_INITIALIZER, isDevMode } from "@angular/core";
 import { inject } from "@vercel/analytics";
+import { injectSpeedInsights } from "@vercel/speed-insights";
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
@@ -14,6 +15,7 @@ export const appConfig: ApplicationConfig = {
       provide: APP_INITIALIZER,
       useFactory: () => {
         inject({ mode: isDevMode() ? 'development' : 'production' });
+        injectSpeedInsights()
       },
     },
   ],
