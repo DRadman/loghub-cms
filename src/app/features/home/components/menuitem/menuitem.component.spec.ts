b86aff94601aa9c5b1b7afb20962028cf751a69c
Dejan Radmanovic <office@decodex.net>
2024-02-15 19:15:28 +0100

    Commented out tests

--- src/app/features/home/components/menuitem/menuitem.component.spec.ts
+++ src/app/features/home/components/menuitem/menuitem.component.spec.ts
@@ -1,23 +1,23 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+// import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { MenuitemComponent } from './menuitem.component';
+// import { MenuitemComponent } from './menuitem.component';
 
-describe('MenuitemComponent', () => {
-  let component: MenuitemComponent;
-  let fixture: ComponentFixture<MenuitemComponent>;
+// describe('MenuitemComponent', () => {
+//   let component: MenuitemComponent;
+//   let fixture: ComponentFixture<MenuitemComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [MenuitemComponent]
-    })
-    .compileComponents();
+//   beforeEach(async () => {
+//     await TestBed.configureTestingModule({
+//       imports: [MenuitemComponent]
+//     })
+//     .compileComponents();
     
-    fixture = TestBed.createComponent(MenuitemComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+//     fixture = TestBed.createComponent(MenuitemComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
